681960c842bc606e415f5c59af13a539736120d8
MYPC <bharadwajsai567@gmail.com>
2024-05-19 14:50:18 +0530

    front end

--- Tera_proj/Servers.tf
+++ Tera_proj/Servers.tf
@@ -6,3 +6,21 @@ resource "aws_instance" "front_end" {
     Name = "frontend_instance"
   }
 }
+
+resource "aws_instance" "back_end" {
+  ami           = "ami-090252cbe067a9e58"
+  instance_type = "t3.small"
+
+  tags = {
+    Name = "backend_instance"
+  }
+}
+
+resource "aws_instance" "front_end" {
+  ami           = "ami-090252cbe067a9e58"
+  instance_type = "t3.small"
+
+  tags = {
+    Name = "mysql_instance"
+  }
+}
