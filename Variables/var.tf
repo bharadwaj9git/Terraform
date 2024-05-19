2cb007fc4a0f7b501542806a81420c188a422068
MYPC <bharadwajsai567@gmail.com>
2024-05-19 16:27:18 +0530

    front end

---
+++ Variables/var.tf
@@ -0,0 +1,43 @@
+# resource "null_resource" "test" {}
+
+variable "x" {
+  default = 10
+}
+
+output "x" {
+  value = var.x
+}
+# lists
+variable "list" {
+  default = [10,
+    "hai",
+    true
+  ]
+}
+
+output "list" {
+  value = var.list
+}
+
+output "list2" {
+  value = var.list[2]
+}
+
+# map variable
+
+variable "map" {
+  default = {
+    course = "DevOps"
+    fruit = "Apple"
+  }
+}
+
+output "l" {
+  value = var.map["course"]
+}
+
+
+variable "Y" {}
+output "Y" {
+  value = var.Y
+}
