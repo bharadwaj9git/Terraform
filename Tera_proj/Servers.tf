82e33a3834e3edd8833085885e5a2280d58a2059
MYPC <bharadwajsai567@gmail.com>
2024-05-19 15:05:47 +0530

    front end

--- Tera_proj/Servers.tf
+++ Tera_proj/Servers.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "front_end" {
   ami           = "ami-090252cbe067a9e58"
   instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-0a83697baaa6fe8b4"]
 
   tags = {
     Name = "frontend_instance"
@@ -10,6 +11,7 @@ resource "aws_instance" "front_end" {
 resource "aws_instance" "back_end" {
   ami           = "ami-090252cbe067a9e58"
   instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-0a83697baaa6fe8b4"]
 
   tags = {
     Name = "backend_instance"
@@ -19,6 +21,7 @@ resource "aws_instance" "back_end" {
 resource "aws_instance" "mysql" {
   ami           = "ami-090252cbe067a9e58"
   instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-0a83697baaa6fe8b4"]
 
   tags = {
     Name = "mysql_instance"
