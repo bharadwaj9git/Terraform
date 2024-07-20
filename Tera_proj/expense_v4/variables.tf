9f3b2d7cc0c67d9f9ab1d65e8c6fcfcb526036c4
MYPC <bharadwajsai567@gmail.com>
2024-07-20 19:39:04 +0530

    dnf

--- Tera_proj/expense_v4/variables.tf
+++ Tera_proj/expense_v4/variables.tf
@@ -18,7 +18,7 @@ variable "ami" {
   default = "ami-041e2ea9402c46c32"
 }
 variable "vpc_security_group_ids" {
-  default = "sg-0a83697baaa6fe8b4"
+  default = ["sg-0a83697baaa6fe8b4"]
 }
 variable "zone_id" {
  default = "Z048613339SQPCT579LPE"
