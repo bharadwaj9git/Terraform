5599f5ae3a23125507f267576d7cd98b24f6b356
MYPC <bharadwajsai567@gmail.com>
2024-07-20 19:50:47 +0530

    dnf

--- Tera_proj/expense_v4/app-module/main.tf
+++ Tera_proj/expense_v4/app-module/main.tf
@@ -10,7 +10,7 @@ Name = var.name
 
 resource "aws_route53_record" "www" {
   zone_id = var.zone_id
-  name    = "$(var.name)-dev.saidevops79.online"
+  name    = "${var.name}-dev.saidevops79.online"
   type    = "A"
   ttl     = 30
   records = [aws_instance.node.private_ip]
