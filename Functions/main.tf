8c0a79f65c519c2a8302a8aa3cc1afe00d057b16
MYPC <bharadwajsai567@gmail.com>
2024-07-20 12:07:15 +0530

    tfvars

--- Functions/main.tf
+++ Functions/main.tf
@@ -1,4 +1,7 @@
 variable "fruits" {}
 output "apple" {
-  value = "apple-quantity -${var.fruits["apple"]["quantity"]} , price -${var.fruits["apple"]["price"]}"
+  value = "apple-quantity -${var.fruits["apple"]["quantity"]}, price -${var.fruits["apple"]["price"]}"
+}
+output "banana" {
+  value = "banana-quantity -${lookup(var.fruits["banana"],"quantity",0.5)} , price -${lookup(var.fruits["banana"],"price",1000)}"
 }
