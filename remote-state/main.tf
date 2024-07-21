91e4ef945d0154bb388f77d20e5ef43cde640786
MYPC <bharadwajsai567@gmail.com>
2024-07-21 14:45:20 +0530

    tfstate

--- remote-state/main.tf
+++ remote-state/main.tf
@@ -3,7 +3,7 @@ resource "null_resource" "test" {}
 terraform {
   backend "s3" {
     bucket = "devsai79terraform"
-    key    = "test/terraform.tfsate"
+    key    = "test/terraform.tfstate"
     region = "us-east-1"
   }
 }
