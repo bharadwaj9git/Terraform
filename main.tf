79ed52c48d36cfcc8414c0cafd408e58a27f20f0
MYPC <bharadwajsai567@gmail.com>
2024-07-27 23:47:54 +0530

    proj

--- main.tf
+++ main.tf
@@ -1,4 +1,10 @@
-module "app" {
+module "frontend" {
+  depends_on = [module.backend]
   source = "git::https://github.com/bharadwaj9git/tf-module.git"
+  instance_type = var.components["frontend"]["instance_type"]
+  name = var.components["frontend"]["name"]
+  env = var.env
+  port_no = var.components["frontend"]["port_no"]
+
 }
 
