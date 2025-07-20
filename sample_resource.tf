8293e50924e8e78e550cb3e9eec983c39d351161
bharadwaj <bharadwajsai567@gmail.com>
2025-07-20 12:51:10 +0530

    first instance

---
+++ sample_resource.tf
@@ -0,0 +1,14 @@
+provider "aws " {
+  region = "us-east-1"
+}
+
+
+resource "aws_instance" "front_end" {
+  ami           = "ami-090252cbe067a9e58"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-0a83697baaa6fe8b4"]
+
+  tags = {
+    Name = "frontend_instance"
+  }
+}
