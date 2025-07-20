18671303850e92a949a608b43efc7a301cbb0e17
bharadwaj <bharadwajsai567@gmail.com>
2025-07-20 12:54:22 +0530

    ami

--- sample_resource.tf
+++ sample_resource.tf
@@ -2,9 +2,8 @@ provider "aws" {
   region = "us-east-1"
 }
 
-
 resource "aws_instance" "front_end" {
-  ami           = "ami-090252cbe067a9e58"
+  ami           = "ami-09c813fb71547fc4f"
   instance_type = "t3.small"
   vpc_security_group_ids = ["sg-0a83697baaa6fe8b4"]
 
